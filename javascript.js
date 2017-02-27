cfa28d9afff9536d074fac34c2ff85b7d69278f8
Johanarrhen <arrhen.98@gmail.com>
2017-02-27 19:39:12 +0100

    1.5

--- javascript.js
+++ javascript.js
@@ -1,15 +1,24 @@
 
-var talet = prompt("skriv ett tal");
 
-talet = parseInt(talet);//gör om till tal
+nummerprint();
+function nummerprint() {
+
+  var talet = prompt("skriv ett tal");
+
+  talet = parseInt(talet);//gör om till tal
 
 
-while (isNaN(talet)){ //Kolla om det är ett tal
+  while (isNaN(talet)){ //Kolla om det är ett tal
 
-    talet = prompt("skriv ett tal");
+      talet = prompt("skriv ett tal");
+  }
+
+  $("#talet").html(talet); //sätt dit talet i paragraf
+  $(".fade").fadeIn();
+
+
 }
 
-$("#talet").html(talet); //sätt dit talet i paragraf
 
 $(".fade").click(function (){ //klicka på texten för att ta bort den
     $(".fade").fadeOut();
